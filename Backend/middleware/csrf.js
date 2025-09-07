9ff5e6247a3d44d70d34f544e06b2b7e10a08e0f
shivam kumar <shivamsharma27107@gmail.com>
2025-09-07 13:00:47 +0530

    hi

--- Backend/middleware/csrf.js
+++ Backend/middleware/csrf.js
@@ -1,22 +1,5 @@
-const csrf = require('csurf');
-
-const csrfProtection = csrf({
-  cookie: {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict'
-  }
-});
-
-const csrfErrorHandler = (err, req, res, next) => {
-  if (err.code === 'EBADCSRFTOKEN') {
-    res.status(403).json({ 
-      message: 'Invalid CSRF token',
-      error: 'CSRF_TOKEN_INVALID'
-    });
-  } else {
-    next(err);
-  }
-};
+// CSRF protection disabled - using dummy middleware
+const csrfProtection = (req, res, next) => next();
+const csrfErrorHandler = (err, req, res, next) => next(err);
 
 module.exports = { csrfProtection, csrfErrorHandler };
